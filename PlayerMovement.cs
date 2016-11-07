f090f902413627851e826466f17f6af6ea9febe7
Kirsty5 <kirsty.bryson5@gmail.com>
2016-11-07 20:18:55 +0000

    Update PlayerMovement.cs

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -14,126 +14,86 @@ public class PlayerMovement : MonoBehaviour {
     {
         bool run = Input.GetKeyDown(KeyCode.E);
 
+	    // If the game is not paused...
         if (GlobalVar.isPaused == false)
         {
             float h = Input.GetAxis("Horizontal");
             float v = Input.GetAxis("Vertical");
-
+	
+		// And the player is not interacting with an object...
             if (GlobalVar.isInteracting == false)
             {
+		    // Determine which direction the player is moving in
                 if (h > 0) { direction = Vector2.right; anim.SetInteger("Facing", 0); }
                 else if (h < 0) { direction = Vector2.left; anim.SetInteger("Facing", 1); }
                 else if (v > 0) { direction = Vector2.up; anim.SetInteger("Facing", 2); }
                 else if (v < 0) { direction = Vector2.down; anim.SetInteger("Facing", 3); }
 
+		    // Set up the new position the player is moving in
                 newPos = new Vector3(h, v, 0) * Time.deltaTime;
 
+		    // Check if the player is running and determine new position based on that
                 if (run)
                     transform.position += newPos * runspeed;
                 else
                     transform.position += newPos * walkspeed;
             }
 
+		// If the player is moving set the right animation
             if ((h == 0 && v == 0) || GlobalVar.isInteracting == true)
                 anim.SetBool("isMoving", false);
             else
                 anim.SetBool("isMoving", true);
 
+		// Check for interactables
             FindInteractable();
-
-            FindPortal();
         }
-        else
+        else // If the game is paused make sure the animation is not moving
             anim.SetBool("isMoving", false);
 
-
+	// Store the direction player is facing in
         GlobalVar.playerFacing = direction;
 
     }
 
     void FindInteractable()
     {
+	    // Set up raycasts for NPCs and other Interactable objects
         RaycastHit2D hitNPC = Physics2D.Raycast(transform.position, direction, 0.25f, LayerMask.GetMask("NPC"));
         RaycastHit2D hitInteract = Physics2D.Raycast(transform.position, direction, 0.5f, LayerMask.GetMask("Interactables"));
 
         bool space = Input.GetKeyDown(KeyCode.Space);
         bool click = Input.GetMouseButtonDown(0);
 
+	    // Check for player input to react to objects
         if (space || click)
         {
-            // If we've hit an NPC
+            // If we've hit an NPC...
             if (hitNPC && GlobalVar.isInteracting == false)
             {
+		    // Store who we're interacting with 
                 GlobalVar.isInteracting = true;
                 GlobalVar.interactingType = "NPC";
                 GlobalVar.interactingWith = hitNPC.collider.gameObject.transform.name.ToString();
             }
-            // If we've hit an object
+            // If we've hit an object...
             else if(hitInteract && !hitNPC && GlobalVar.isInteracting == false)
             {
+		    // Store which object we've interacted with
                 string interact = hitInteract.transform.name;
                 GlobalVar.interactingType = "Object";
                 GlobalVar.isInteracting = true;
                 GlobalVar.interactingWith = interact;
             }
+		// If we've finished interacting with an object
             else if (GlobalVar.doneInteracting == true)
             {
+		    // Clear the data for who or what we've interacted with
                 GlobalVar.isInteracting = false;
                 GlobalVar.doneInteracting = false;
             }
 
         }
         
-    }
-
-    void FindPortal()
-    {
-        RaycastHit2D hitPortal = Physics2D.Raycast(transform.position, direction, 0.3f, LayerMask.GetMask("Portal"));
-        if(hitPortal)
-        {
-            string door = hitPortal.transform.name;
-
-            print("Collided with " + door + ". Moving to new Location.");
-
-            if(door == "Players House Enter")
-            {
-                transform.position = new Vector3(0,13.0f,0);
-            }
-
-            if (door == "Players House Exit")
-            {
-                transform.position = new Vector3(0, 0.5f, 0);
-            }
-
-            if (door == "Serenas House Enter")
-            {
-                transform.position = new Vector3(15.4f, 13.4f, 0);
-            }
-
-            if (door == "Serenas House Exit")
-            {
-                transform.position = new Vector3(5.0f, 0.5f, 0);
-            }
-
-            if (door == "Maisies Shop Enter")
-            {
-                transform.position = new Vector3(-13.2f, 13.2f, 0);
-            }
-
-            if (door == "Maisies Shop Exit")
-            {
-                transform.position = new Vector3(-5.0f, 0.5f, 0);
-            }
-
-            if (door == "Library Enter")
-            {
-                transform.position = new Vector3(-27.5f, -1.8f, 0);
-            }
-
-            if (door == "Library Exit")
-            {
-                transform.position = new Vector3(0.0f, -5.0f, 0);
-            }
-        }
     }
 }
